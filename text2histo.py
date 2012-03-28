20c8a27c296b4629c1868700ae8a02d440440c71
Martin Dengler <martin@martindengler.com>
2012-03-29 00:33:00 +0800

    refactor into functions

--- text2histo.py
+++ text2histo.py
@@ -12,59 +12,79 @@ import os
 import sys
 
 
-if "--help" in sys.argv:
-    sys.stderr.print(__doc__)
-    sys.exit(1)
+
+def get_counts(lines):
+    """returns a dict of integer -> occurrence count"""
+
+    counts = {}
+
+    for line in lines:
+        line = line.strip()
+
+        try:
+            count = int(line)
+        except Exception, msg:
+            count = len(line)
+
+        if count not in counts:
+            counts[count] = 0
+
+        counts[count] = counts[count] + 1
+
+    return counts
 
 
-counts = {}
+def get_bin_contents(counts):
+    """returns a list of bin_start, bin_end, size lists"""
+    max_c = max(counts.keys())
+    min_c = min(counts.keys())
 
-if len(sys.argv[1:]) > 1:
-    lines = sys.argv[1:]
-else:
-    lines = sys.stdin.readlines()
+    bins = min(20, len(counts.keys()))
+    bin_range = (max_c - min_c) + 1
+    bin_increment = bin_range / math.ceil(float(bins))
 
-for line in lines:
-    line = line.strip()
-    try:
-        count = int(line)
-    except Exception, msg:
-        count = len(line)
+    bin_start = max_c
 
-    if count not in counts:
-        counts[count] = 0
-    counts[count] = counts[count] + 1
+    bin_contents = []
+    for i in range(bins):
+        if i == bins - 1:
+            bin_end = min_c
+        else:
+            bin_end = bin_start - bin_increment
+        size = sum([counts[c] for c in counts
+                    if (c <= bin_start) and (c > bin_end)])
+        bin_contents.append([bin_start, bin_end, size])
+        bin_start = bin_end
 
-max_c = max(counts.keys())
-min_c = min(counts.keys())
+    return bin_contents
 
-bins = min(20, len(counts.keys()))
-bin_range = (max_c - min_c) + 1
-bin_increment = bin_range / math.ceil(float(bins))
 
-bin_start = max_c
+def print_histogram_bins(bin_contents):
+    bin_start_and_ends = reduce(lambda a, b: a + b,
+                                [[s, e] for s, e, size in bin_contents], [])
+    bin_label_padding = max(map(len, map(str, bin_start_and_ends))) + 1
+    size_padding = max([len(str(size)) for s, e, size in bin_contents])
+    format_str = "%%%ds .. %%%ds | [%%%ds] %%s\n" % (bin_label_padding,
+                                                     bin_label_padding,
+                                                     size_padding)
 
-bin_contents = []
-for i in range(bins):
-    if i == bins - 1:
-        bin_end = min_c
+    for bin_start, bin_end, size in bin_contents:
+        sys.stdout.write(format_str % (bin_start,
+                                       bin_end,
+                                       size,
+                                       "#" * size))
+
+if __name__ == "__main__":
+
+    if "--help" in sys.argv:
+        sys.stderr.write(__doc__)
+        sys.exit(1)
+
+    if len(sys.argv[1:]) > 1:
+        lines = sys.argv[1:]
     else:
-        bin_end = bin_start - bin_increment
-    size = sum([counts[c] for c in counts
-                if (c <= bin_start) and (c > bin_end)])
-    bin_contents.append([bin_start, bin_end, size])
-    bin_start = bin_end
+        lines = sys.stdin.readlines()
 
-bin_start_and_ends = reduce(lambda a, b: a + b,
-                            [[s, e] for s, e, size in bin_contents], [])
-bin_label_padding = max(map(len, map(str, bin_start_and_ends))) + 1
-size_padding = max([len(str(size)) for s, e, size in bin_contents])
-format_str = "%%%ds .. %%%ds | [%%%ds] %%s\n" % (bin_label_padding,
-                                                 bin_label_padding,
-                                                 size_padding)
-
-for bin_start, bin_end, size in bin_contents:
-    sys.stdout.write(format_str % (bin_start,
-                                   bin_end,
-                                   size,
-                                   "#" * size))
+    counts = get_counts(lines)
+    bin_contents = get_bin_contents(counts)
+    print_histogram_bins(bin_contents)
